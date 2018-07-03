087f112d7a2c72700fd088fbae5ef495e2c62090
Sarah Jacob <smjacob91@gmail.com>
2018-07-03 10:49:12 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,16 +16,11 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elseif phrase==phrase.downcase
+  elseif phrase == phrase.downcase
     return  "HUH?! SPEAK UP, SONNY!"
-    
-    
-    
-    
-  elseif phrase == phrase.upcase
+  else phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else
-    return "HUH?! SPEAK UP, SONNY!"
   end
 end
     
+  
