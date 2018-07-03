185a8795e894ed896118f5c5a4f60d0eba11d634
Sarah Jacob <smjacob91@gmail.com>
2018-07-03 10:40:12 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   elseif phrase == "I LOVE YOU GRANDMA!"
-    return n"I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
     return "NO, NOT SINCE 1938!"
   end
